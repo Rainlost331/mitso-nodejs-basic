62aa72205930efd50edea06dcb026fa9e7b16dec
Rainlost331 <artem2004d04@mail.ru>
2025-04-24 19:12:09 +0300

    solved Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const sourceFile = join(__dirname, 'files', 'fileToCompress.txt');
+    const archiveFile = join(__dirname, 'files', 'archive.gz');
+    
+    const readStream = createReadStream(sourceFile);
+    const writeStream = createWriteStream(archiveFile);
+    const gzip = createGzip();
+    
+    readStream.pipe(gzip).pipe(writeStream);
+    
+    return new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+        readStream.on('error', reject);
+    });
 };
 
 await compress();
