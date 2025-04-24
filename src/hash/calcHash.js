e040da18208c73a752d140715e14a23fca21867f
Rainlost331 <artem2004d04@mail.ru>
2025-04-24 18:39:40 +0300

    solved Modules & Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    
+    try {
+        const fileContent = await readFile(filePath);
+        const hash = createHash('sha256').update(fileContent).digest('hex');
+        console.log(hash);
+    } catch (err) {
+        console.error('Error calculating hash:', err.message);
+        throw err;
+    }
 };
 
 await calculateHash();
